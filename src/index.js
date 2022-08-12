defcba2401fcf1832b3494cc6778f43a35506bd6
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2022-08-12 20:19:10 -0300

    Listando o extrato do cliente

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ const app = express();
 
 app.use(express.json());
 
-const costomers = [];
+const customers = [];
 
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body
 
-    const customerAlreadyExists = costomers.some(
-        (costomers) => costomers.cpf === cpf
+    const customerAlreadyExists = customers.some(
+        (customers) => customers.cpf === cpf
     )
 
     if (customerAlreadyExists) {
         return response.status(400).json({ error: "customer already exists!!!" });
     }
 
-    costomers.push({
+    customers.push({
         cpf,
         name,
         id: uuidv4(),
@@ -26,5 +26,13 @@ app.post("/account", (request, response) => {
     });
 
     return response.status(201).send();
+});
+
+app.get("/statement/:cpf", (request, response) => {
+    const { cpf } = request.params;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    return response.json(customer.statement);
 });
 app.listen(3333);
