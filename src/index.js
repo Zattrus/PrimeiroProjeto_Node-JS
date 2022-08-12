ebb974b999d7192b2a0ec694207eee0247e66f3e
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2022-08-12 20:04:37 -0300

    Definindo a criação de so um CPF

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-app.listen(4242);
+app.use(express.json());
+
+const costomers = [];
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body
+
+    const customerAlreadyExists = costomers.some(
+        (costomers) => costomers.cpf === cpf
+    )
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "customer already exists!!!" });
+    }
+
+    costomers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+
+    return response.status(201).send();
+});
+app.listen(3333);
